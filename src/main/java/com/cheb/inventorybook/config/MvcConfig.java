9e4e54a431cc80b775f0c671230721f8ae5bf8b5
ShemiareiD <DShemiar@sk.ibm.com>
2018-08-01 20:13:10 +0300

    Temp save

--- src/main/java/com/cheb/inventorybook/config/MvcConfig.java
+++ src/main/java/com/cheb/inventorybook/config/MvcConfig.java
@@ -3,18 +3,22 @@ package com.cheb.inventorybook.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer{
 
-    public MvcConfig(){
-        super();
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/");
     }
 
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
+//    @Override
+//    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//        configurer.enable();
+//    }
 }
